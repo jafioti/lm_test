6619a989c8594f17c4f1566fa81b85f0471b6fa7
Joe Fioti <jafioti@gmail.com>
2023-03-08 10:18:34 -0600

    Upgraded to newer dfdx

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(clippy::type_complexity)]
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
+
+use colored::Colorize;
 use dataflow::{
     dataloader::Dataloader,
     pipeline::*,
@@ -9,80 +11,187 @@ use dataflow_nlp::{
     tokenization::{Tokenizer, WordpieceTokenizer},
     vocab::{Vocab, WordPieceVocab},
 };
-use dfdx::{prelude::*, optim::{Sgd, SgdConfig, Momentum}};
+use dfdx::{prelude::{*, storage_traits::TensorToArray}, optim::{Adam, AdamConfig}};
 
 #[allow(unused)]
 mod bar;
+pub use bar::*;
 #[allow(unused)]
 #[allow(clippy::uninlined_format_args)]
 mod indicatif;
+mod position_encoding;
 
-use std::mem::transmute;
+type Model<const VOCAB: usize, const EMBED: usize, const LAYERS: usize, const HEADS: usize, const SEQ: usize> = (
+    Embedding<VOCAB, EMBED>,
+    // position_encoding::builder::LearnedPositionalEmbedding<SEQ, EMBED>,
+    TransformerEncoder<EMBED, HEADS, {EMBED * 2}, LAYERS>,
+    Linear<EMBED, VOCAB>,
+);
 
-use crate::bar::ExponentialAverage;
+type BuiltModel<const VOCAB: usize, const EMBED: usize, const LAYERS: usize, const HEADS: usize, const SEQ: usize, E, D> = (
+    dfdx::nn::modules::Embedding<VOCAB, EMBED, E, D>,
+    // position_encoding::LearnedPositionalEmbedding<SEQ, EMBED, E, D>,
+    dfdx::nn::modules::TransformerEncoder<EMBED, HEADS, {EMBED * 2}, LAYERS, E, D>,
+    dfdx::nn::modules::Linear<EMBED, VOCAB, E, D>,
+);
+
+// Training
+const BATCH_SIZE: usize = 32;
+const BATCH_ACCUM: usize = 1;
+const LR: f32 = 3e-4;
+
+// Model
+const LAYERS: usize = 8;
+const SEQ_LEN: usize = 25;
+const EMBED_DIM: usize = 512;
+const HEADS: usize = 16;
 
 fn main() {
-    let mut train_dataset =
-        build_dataset::<5, 1>("/home/jafioti/Datasets/openwebtext", 0, 500_000);
+    let mut train_dataset = build_dataset::<SEQ_LEN, BATCH_SIZE>("/home/jafioti/Datasets/openwebtext", 100_000, 500_000);
+    let mut test_dataset = build_dataset::<SEQ_LEN, BATCH_SIZE>("/home/jafioti/Datasets/openwebtext", 0, 10_000);
     let dev: Cuda = Default::default();
-    let embedding: Tensor<Rank2<30527, 40>, _, Cuda> = dev.sample_uniform();
-    let mut model: (TransformerEncoder<40, 1, 100, 2, Cuda>, Linear<40, 30527, Cuda>) = dev.build_module();
-    let mut sgd: Sgd<(TransformerEncoder<40, 1, 100, 2, Cuda>, Linear<40, 30527, Cuda>), Cuda> = Sgd::new(SgdConfig {
-        lr: 1e-3,
-        momentum: Some(Momentum::Nesterov(0.9)),
-        weight_decay: None,
+    let mut model = Model::<30527, EMBED_DIM, LAYERS, HEADS, SEQ_LEN>::build_on_device(&dev);
+    // model.load("test_save.npz").unwrap();
+    let mut opt: Adam<_, _, _> = Adam::new(&model, AdamConfig {
+        lr: LR,
+        ..Default::default()
     });
+    let mut tensorboard = Tensorboard::new("logdir");
 
-    // Tracking
-    let total_len = train_dataset.len();
-    let bar = bar::train_progress_bar(train_dataset.len() as u64);
-    let mut loss_avg: ExponentialAverage<f32> = bar::ExponentialAverage::new();
+    println!("Model Parameters: {}", pretty_print_num(model.num_trainable_params()));
 
-    test(&embedding, &model, dev.clone());
+    generate(&model, &dev, 50);
+    for epoch in 0..3 {
+        println!("{}", format!("Epoch {}", epoch + 1).bold().cyan());
+        train_epoch(&mut model, &mut train_dataset, &mut opt, &dev, &mut tensorboard);
+        println!("Test PPL: {}", test_epoch(&model, &mut test_dataset, &dev));
 
-    for ((input, target), left) in train_dataset.iter_len() {
-        let embedded: Tensor<Rank3<1, 5, 40>, _, Cuda> = embedding.clone().gather(dev.tensor(input));
-        // let embedded: Tensor3D<1, 5, 40, _> = embedded.reshape();
-        let output: Tensor<Rank3<1, 5, 30527>, _, Cuda, OwnedTape<Cuda>> = model.forward_mut(embedded.trace());
-        let output: Tensor<Rank2<5, 30527>, _, Cuda, OwnedTape<Cuda>> = output.reshape();
+        generate(&model, &dev, 50);
 
-        let target: Tensor<(Const<5>, Const<30527>), _, Cuda> = one_hot_encode(&unsafe {transmute(target)}, dev.clone());
-        // let loss = cross_entropy_with_logits_loss(output, target);
-        let loss = mse_loss(output.log_softmax::<Axis<1>>(), target);
-        let loss_val: f32 = loss.array();
-        loss_avg.update(loss_val);
-        bar.set_position((total_len - left) as u64);
-        bar.set_message(format!("PPL: {:.2}", loss_avg.value));
-
-        let gradients = loss.backward();
-        sgd.update(&mut model, gradients).unwrap();
+        // if let Err(e) = model.save(&format!("model-{epoch}.npz")) {
+        //     println!("Error saving model: {e:?}");
+        // }
     }
-    drop(bar);
-    println!();
-    println!();
-    println!();
-
-    test(&embedding, &model, dev.clone());
-    test(&embedding, &model, dev);
 }
 
-fn test(embedding: &Tensor<Rank2<30527, 40>, f32, Cuda>, model: &(TransformerEncoder<40, 1, 100, 2, Cuda>, Linear<40, 30527, Cuda>), dev: Cuda) {
-    let input = "Hello there, how are you doing";
-    let (tokenizer, vocab) = (WordpieceTokenizer::load(), WordPieceVocab::load());
-    let tokens = tokenizer.tokenize(input);
-    let indexes = vocab.indexes_from_tokens(&tokens).unwrap();
+fn train_epoch<const LAYERS: usize, const SEQ: usize, const VOCAB: usize, const EMBED: usize, const HEADS: usize, D: Device<f32>>(
+    model: &mut BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>, 
+    dataset: &mut Dataloader<([[usize; SEQ]; BATCH_SIZE], [[usize; SEQ]; BATCH_SIZE])>, 
+    opt: &mut Adam<BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>, f32, D>,
+    dev: &D,
+    tensorboard: &mut Tensorboard,
+) where 
+    D: Device<f32> + dfdx::optim::AdamKernel<f32> + std::fmt::Debug + dfdx::tensor::TensorFrom<[[usize; SEQ]; BATCH_SIZE], Rank2<BATCH_SIZE, SEQ>, usize>,
+    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>: ModuleMut<Tensor<Rank2<BATCH_SIZE, SEQ>, usize, D, OwnedTape<f32, D>>, Output = Tensor<Rank3<BATCH_SIZE, SEQ, VOCAB>, f32, D, OwnedTape<f32, D>>>,
+    Tensor<(), f32, D, OwnedTape<f32, D>>: AsArray<Array = f32>,
+    [(); EMBED * 2]:
+{
+    let total_len = dataset.len();
+    let bar = train_progress_bar(dataset.len() as u64);
+    let mut loss_avg = ExponentialAverage::<f32>::with_beta(0.999);
+    // let mut gradients = None;
+    for ((input, target), left) in dataset.iter_len() {
+        let output = match model.try_forward_mut(dev.tensor(input).traced()) {
+            Ok(o) => o,
+            Err(e) => {
+                println!("Forward Error: {e:?}");
+                continue;
+            }
+        };
 
-    let indexes: [usize; 5] = indexes[..5].try_into().unwrap();
-    let embedded: Tensor<Rank2<5, 40>, _, Cuda> = embedding.clone().gather(dev.tensor(indexes));
-    let output = model.forward(embedded);
-    let indexes: Vec<usize> = output.array().iter().map(|dist| dist
-        .iter()
-        .enumerate()
-        .max_by(|(_, a), (_, b)| a.total_cmp(b))
-        .map(|(index, _)| index)
-        .unwrap()).collect();
-    
-    println!("Result: {}", vocab.tokens_from_indexes(&indexes).unwrap().join(" "));
+        let loss = match one_hot_encode(&target, dev)
+            .map(|t| try_cross_entropy_with_logits_loss(output, t)) {
+            Ok(Ok(l)) => l,
+            r => {
+                println!("Loss Error: {r:?}");
+                continue;
+            }
+        };
+        
+        let ppl = loss.array().exp();
+        loss_avg.update(ppl);
+        bar.set_position((total_len - left) as u64);
+        bar.set_message(format!("PPL: {:.2}", loss_avg.value));
+        tensorboard.record("ppl", loss_avg.value, BATCH_SIZE);
+
+        let gradients = loss.backward();
+        // println!("\n{:?}", gradients.get(&model.1.weight).mean::<Rank0, _>().as_vec());
+        // println!("{:?}\n", gradients.get(&model.3.weight).mean::<Rank0, _>().as_vec());
+
+        if let Err(e) = opt.update(model, &gradients) {
+            println!("Update error: {e:?}");
+        }
+
+        // if tensorboard.iter * BATCH_SIZE % 100_000 == 0 {
+        //     if let Err(e) = model.save(&format!("step_{}.npz", tensorboard.iter * BATCH_SIZE)) {
+        //         println!("Error saving model: {e:?}");
+        //     }
+        // }
+    }
+    drop(bar);
+
+    println!("Train PPL: {}", loss_avg.value);
+}
+
+fn test_epoch<const LAYERS: usize, const SEQ: usize, const VOCAB: usize, const EMBED: usize, const HEADS: usize, D: Device<f32>>(
+    model: &BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>, 
+    dataset: &mut Dataloader<([[usize; SEQ]; BATCH_SIZE], [[usize; SEQ]; BATCH_SIZE])>, 
+    dev: &D
+) -> f32
+where 
+    D: Device<f32> + dfdx::tensor::TensorFrom<[[usize; SEQ]; BATCH_SIZE], Rank2<BATCH_SIZE, SEQ>, usize>,
+    Tensor<(), f32, D, NoneTape>: AsArray<Array = f32>,
+    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>: Module<Tensor<Rank2<BATCH_SIZE, SEQ>, usize, D, NoneTape>, Output = Tensor<Rank3<BATCH_SIZE, SEQ, VOCAB>, f32, D, NoneTape>>,
+    [(); EMBED * 2]:
+{
+    let total_len = dataset.len();
+    let bar = test_progress_bar(dataset.len() as u64);
+    let mut loss_avg = ExponentialAverage::<f32>::new();
+    let mut losses = Vec::with_capacity(total_len);
+    for ((input, target), left) in dataset.iter_len() {
+        let output = model.forward(dev.tensor(input));
+
+        let loss: Tensor<(), f32, D> = cross_entropy_with_logits_loss(output, one_hot_encode(&target, dev).unwrap());
+        let loss = loss.array().exp();
+        losses.push(loss);
+        loss_avg.update(loss);
+        bar.set_position((total_len - left) as u64);
+        bar.set_message(format!("PPL: {:.2}", loss_avg.value));
+    }
+    drop(bar);
+
+    return losses.iter().sum::<f32>() / (losses.len() as f32)
+}
+
+fn generate<const LAYERS: usize, const VOCAB: usize, const EMBED: usize, const HEADS: usize, D: Device<f32>>(
+    model: &BuiltModel<VOCAB, EMBED, LAYERS, HEADS, 25, f32, D>, 
+    dev: &D,
+    num_tokens: usize,
+) 
+where 
+    D: TensorToArray<(dfdx::shapes::Const<25>, dfdx::shapes::Const<VOCAB>), f32, Array = [[f32; VOCAB]; 25]>,
+    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, 25, f32, D>: Module<Tensor<Rank1<25>, usize, D>, Output = Tensor<Rank2<25, VOCAB>, f32, D>>,
+    D: Device<f32> + dfdx::tensor::TensorFrom<[usize; 25], Rank1<25>, usize> + TensorFrom<[[f32; 25]; 25], Rank2<25, 25>, f32>,
+    [(); EMBED * 2]:
+{
+    let input = "Hi, how are you doing today? I'd like you to meet my friend Fred. He's a botonist, but also the first man to ever walk on the surface of".to_lowercase();
+    let (tokenizer, vocab) = (<WordpieceTokenizer as Tokenizer>::load(), <WordPieceVocab as Vocab>::load());
+    let tokens = tokenizer.tokenize(&input);
+    let mut indexes = vocab.indexes_from_tokens(&tokens).unwrap();
+
+    for _ in 0..num_tokens {
+        let window: [usize; 25] = indexes[indexes.len() - 25..].try_into().unwrap();
+        let input: Tensor<_, _, D> = dev.tensor(window);
+        let output = model.forward(input);
+        let index = output.array().last().unwrap()
+            .iter()
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.total_cmp(b))
+            .map(|(index, _)| index)
+            .unwrap();
+        indexes.push(index);
+    }
+    println!("{} {}", "Generation:".bold(), tokenizer.untokenize(vocab.tokens_from_indexes(&indexes).unwrap()));
 }
 
 fn build_dataset<const SEQ: usize, const BATCH: usize>(
@@ -101,7 +210,8 @@ fn build_dataset<const SEQ: usize, const BATCH: usize>(
             (WordpieceTokenizer::load(), WordPieceVocab::load()),
             |strings: Vec<String>, (tokenizer, vocab)| {
                 let strings = strings.into_iter()
-                    .filter(|s| !s.is_empty() && !s.contains("\\00")).map(|s| s.replace(['\\', '/', '"'], "")).collect();                
+                    .filter(|s| !s.is_empty() && !s.contains("\\00"))
+                    .map(|s| s.to_lowercase().replace(['\\', '/', '"'], "")).collect();                
                 let tokens = tokenizer.batch_tokenize(strings);
                 let indexes = vocab.batch_indexes_from_tokens(&tokens).unwrap();
 
@@ -127,16 +237,15 @@ fn build_dataset<const SEQ: usize, const BATCH: usize>(
     Dataloader::new(pipeline).load_block_size(1_000_000)
 }
 
-fn one_hot_encode<const B: usize, const T: usize>(
-    labels: &[usize; B],
-    dev: Cuda
-) -> Tensor<(Const<B>, Const<T>), f32, Cuda> {
-    let mut data = vec![0.; B * T];
-    for (i, l) in labels.iter().enumerate() {
-        data[i* *l] = 1.;
+fn one_hot_encode<const B: usize, const S: usize, const V: usize, D: Device<f32>>(
+    labels: &[[usize; S]; B],
+    dev: &D
+) -> Result<Tensor<(Const<B>, Const<S>, Const<V>), f32, D>, D::Err> {
+    let mut data = vec![0.; B * S * V];
+    for (b, batch) in labels.iter().enumerate() {
+        for (i, l) in batch.iter().enumerate() {
+            data[b*S*V + i*V + *l] = 1.;
+        }
     }
-
-    let mut tensor = dev.zeros();
-    tensor.copy_from(&data);
-    tensor
+    dev.try_tensor_from_vec(data, (Const::<B>, Const::<S>, Const::<V>))
 }
---
+++ src/position_encoding.rs
@@ -0,0 +1,110 @@
+use dfdx::prelude::tensor_collection::{ModuleVisitor, TensorOptions};
+use dfdx::{gradients::Tape, prelude::tensor_collection::TensorCollection};
+use dfdx::prelude::*;
+use rand_distr::Uniform;
+
+pub mod builder {
+    #[derive(Debug)]
+    pub struct LearnedPositionalEmbedding<const MAX_LEN: usize, const DIM: usize>;
+}
+
+impl<const V: usize, const M: usize, E: Dtype, D: Device<E>> BuildOnDevice<D, E>
+    for builder::LearnedPositionalEmbedding<V, M>
+where
+LearnedPositionalEmbedding<V, M, E, D>: BuildModule<D, E>,
+{
+    type Built = LearnedPositionalEmbedding<V, M, E, D>;
+    fn try_build_on_device(device: &D) -> Result<Self::Built, D::Err> {
+        Self::Built::try_build(device)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct LearnedPositionalEmbedding<
+    const MAX_LEN: usize,
+    const DIM: usize,
+    E: Dtype,
+    D: DeviceStorage,
+> {
+    /// Learned positonal embeddings
+    pub weight: Tensor<Rank2<MAX_LEN, DIM>, E, D>,
+}
+
+/// Pass in an unbatched pre-embedded sequence, add positional embeddings in
+impl<const MAX_LEN: usize, const DIM: usize, D: Device<f32>, T: Tape<f32, D>>
+    Module<Tensor<(Const<MAX_LEN>, Const<DIM>), f32, D, T>>
+    for LearnedPositionalEmbedding<MAX_LEN, DIM, f32, D>
+where
+    D: TensorFromVec<usize>,
+{
+    type Output = Tensor<(Const<MAX_LEN>, Const<DIM>), f32, D, T>;
+    type Error = D::Err;
+    fn try_forward(&self, input: Tensor<(Const<MAX_LEN>, Const<DIM>), f32, D, T>) -> Result<Self::Output, Self::Error> {
+        let (input, tape) = input.split_tape();
+        self.weight
+            .clone()
+            .put_tape(tape)
+            .try_add(input)
+    }
+}
+
+impl<const V: usize, const M: usize, E: Dtype, D: DeviceStorage> NonMutableModule
+    for LearnedPositionalEmbedding<V, M, E, D>{}
+
+impl<const MAX_LEN: usize, const DIM: usize, BATCH: Dim, D: Device<f32>, T: Tape<f32, D>>
+    Module<Tensor<(BATCH, Const<MAX_LEN>, Const<DIM>), f32, D, T>>
+    for LearnedPositionalEmbedding<MAX_LEN, DIM, f32, D>
+where
+    D: TensorFromVec<usize>,
+    D: dfdx::tensor::storage_traits::TensorToArray<(dfdx::shapes::Const<MAX_LEN>, dfdx::shapes::Const<DIM>), f32>,
+{
+    type Output = Tensor<(BATCH, Const<MAX_LEN>, Const<DIM>), f32, D, T>;
+    type Error = D::Err;
+    fn try_forward(&self, input: Tensor<(BATCH, Const<MAX_LEN>, Const<DIM>), f32, D, T>) -> Result<Self::Output, Self::Error> {
+        let (input, tape) = input.split_tape();
+        self.weight
+            .clone()
+            .put_tape(tape)
+            .try_broadcast_like(input.shape())?
+            .try_add(input)
+    }
+}
+
+impl<const MAX_LEN: usize, const DIM: usize, D: Device<f32>> BuildModule<D, f32>
+    for LearnedPositionalEmbedding<MAX_LEN, DIM, f32, D>
+{
+    fn try_build(device: &D) -> Result<Self, D::Err> {
+        let bound = 1. / (MAX_LEN as f32).sqrt();
+        let weight = device.try_sample(Uniform::new(-bound, bound))?;
+        Ok(Self {
+            weight,
+        })
+    }
+}
+
+impl<const MAX_LEN: usize, const DIM: usize, D1: Device<f32>, D2: Device<f32>> ToDevice<D2>
+    for LearnedPositionalEmbedding<MAX_LEN, DIM, f32, D1>
+{
+    type Output = LearnedPositionalEmbedding<MAX_LEN, DIM, f32, D2>;
+    fn to_device(&self, device: &D2) -> Self::Output {
+        LearnedPositionalEmbedding {
+            weight: self.weight.to_device(device),
+        }
+    }
+}
+impl<const C: usize, const M: usize, D: SampleTensor<f32> + Device<f32>>
+    TensorCollection<f32, D> for LearnedPositionalEmbedding<C, M, f32, D>
+{
+    fn iter_tensors<V: ModuleVisitor<Self, f32, D>>(visitor: &mut V) -> Result<(), V::Err> {
+        visitor.visit_tensor(
+            "weight",
+            |s| &s.weight,
+            |s| &mut s.weight,
+            TensorOptions::reset_with(|t| {
+                let b = 1. / (C as f32).sqrt();
+                t.try_fill_with_distr(Uniform::new(-b, b))
+            }),
+        )
+
+    }
+}
