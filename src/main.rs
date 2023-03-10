14aa084d0eb378e6a3802b48564968886b8291aa
Joe Fioti <jafioti@gmail.com>
2023-03-10 07:39:18 -0600

    Cleanups

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use dataflow_nlp::{
     tokenization::{Tokenizer, WordpieceTokenizer},
     vocab::{Vocab, WordPieceVocab},
 };
-use dfdx::{prelude::*, optim::{Adam, AdamConfig}, gradients::Tape};
+use dfdx::{prelude::*, nn::modules, optim::{Adam, AdamConfig}, gradients::Tape};
 
 #[allow(unused)]
 mod bar;
@@ -29,26 +29,26 @@ type Model<const VOCAB: usize, const EMBED: usize, const LAYERS: usize, const HE
 );
 
 type BuiltModel<const VOCAB: usize, const EMBED: usize, const LAYERS: usize, const HEADS: usize, const SEQ: usize, E, D> = (
-    dfdx::nn::modules::Embedding<VOCAB, EMBED, E, D>,
+    modules::Embedding<VOCAB, EMBED, E, D>,
     position_encoding::LearnedPositionalEmbedding<SEQ, EMBED, E, D>,
-    dfdx::nn::modules::TransformerEncoder<EMBED, HEADS, {EMBED * 2}, LAYERS, E, D>,
-    dfdx::nn::modules::Linear<EMBED, VOCAB, E, D>,
+    modules::TransformerEncoder<EMBED, HEADS, {EMBED * 2}, LAYERS, E, D>,
+    modules::Linear<EMBED, VOCAB, E, D>,
 );
 
 // Training
-const BATCH_SIZE: usize = 32;
-const BATCH_ACCUM: usize = 2;
-const LR: f32 = 2e-5;
+const BATCH_SIZE: usize = 64;
+const BATCH_ACCUM: usize = 1;
+const LR: f32 = 3e-5;
 
 // Model
 const LAYERS: usize = 8;
 const SEQ_LEN: usize = 25;
-const EMBED_DIM: usize = 1024;
-const HEADS: usize = 16;
+const EMBED_DIM: usize = 512;
+const HEADS: usize = 8;
 
 fn main() {
-    let mut train_dataset = build_dataset::<SEQ_LEN, BATCH_SIZE>("/home/jafioti/Datasets/openwebtext", 100_000, 5_000_000);
-    let mut test_dataset = build_dataset::<SEQ_LEN, BATCH_SIZE>("/home/jafioti/Datasets/openwebtext", 0, 10_000);
+    let mut train_dataset = build_dataset::<SEQ_LEN, BATCH_SIZE>("/home/jafioti/Datasets/openwebtext", 100_000, 20_000_000);
+    let mut test_dataset = build_dataset::<SEQ_LEN, BATCH_SIZE>("/home/jafioti/Datasets/openwebtext", 0, 100_000);
     let dev: Cuda = Default::default();
     let mut model = Model::<30527, EMBED_DIM, LAYERS, HEADS, SEQ_LEN>::build_on_device(&dev);
     // model.load("epoch-0.npz").unwrap();
@@ -68,8 +68,8 @@ fn main() {
 
         generate(&model, &dev, 50);
 
-        if let Err(e) = model.save(&format!("epoch-{epoch}.npz")) {
-            println!("Error saving model: {e:?}");
+        if let Err(e) = model.save(&format!("checkpoints/epoch-{epoch}.npz")) {
+            println!("{} {e:?}", "Error Saving Model:".bold().red());
         }
     }
 }
@@ -81,9 +81,13 @@ fn train_epoch<const LAYERS: usize, const SEQ: usize, const VOCAB: usize, const 
     dev: &D,
     tensorboard: &mut Tensorboard,
 ) where 
-    D: Device<f32> + std::fmt::Debug + dfdx::tensor::TensorFrom<[[usize; SEQ]; BATCH_SIZE], Rank2<BATCH_SIZE, SEQ>, usize>,
-    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>: Module<Tensor<Rank2<BATCH_SIZE, SEQ>, usize, D, OwnedTape<f32, D>>, Output = Tensor<Rank3<BATCH_SIZE, SEQ, VOCAB>, f32, D, OwnedTape<f32, D>>>,
+    D: Device<f32> + std::fmt::Debug + dfdx::tensor::TensorFrom<[[usize; SEQ]; BATCH_SIZE], Rank2<BATCH_SIZE, SEQ>, usize>
+     + dfdx::tensor::TensorFrom<[usize; SEQ], Rank1<SEQ>, usize> + dfdx::tensor::TensorFromVec<usize>,
+    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>: 
+        Module<Tensor<Rank2<BATCH_SIZE, SEQ>, usize, D, OwnedTape<f32, D>>, Output = Tensor<Rank3<BATCH_SIZE, SEQ, VOCAB>, f32, D, OwnedTape<f32, D>>>
+        + Module<Tensor<Rank1<SEQ>, usize, D>, Output = Tensor<Rank2<SEQ, VOCAB>, f32, D>>,
     Tensor<(), f32, D, OwnedTape<f32, D>>: AsArray<Array = f32>,
+    Tensor<(dfdx::shapes::Const<SEQ>, dfdx::shapes::Const<VOCAB>), f32, D>: AsArray<Array = [[f32; VOCAB]; SEQ]>,
     O: Optimizer<BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>, D, f32>,
     [(); EMBED * 2]:
 {
@@ -92,14 +96,14 @@ fn train_epoch<const LAYERS: usize, const SEQ: usize, const VOCAB: usize, const 
     let mut loss_avg = ExponentialAverage::<f32>::with_beta(0.999);
     let mut gradients = Some(model.alloc_grads());
     for ((input, target), left) in dataset.iter_len() {
-        let input = match gradients.take() {
+        let input = match Option::take(&mut gradients) {
             Some(g) => dev.tensor(input).trace_into(g),
             None => dev.tensor(input).traced()
         };
         let output = match model.try_forward(input) {
             Ok(o) => o,
             Err(e) => {
-                println!("Forward Error: {e:?}");
+                println!("{} {e:?}\n", "Forward Error:".bold().red());
                 continue;
             }
         };
@@ -108,35 +112,37 @@ fn train_epoch<const LAYERS: usize, const SEQ: usize, const VOCAB: usize, const 
             .map(|t| try_cross_entropy_with_logits_loss(output, t)) {
             Ok(Ok(l)) => l,
             r => {
-                println!("Loss Error: {r:?}");
+                println!("{} {r:?}\n", "Loss Error:".bold().red());
                 continue;
             }
         };
         
-        let ppl = loss.array().exp();
-        loss_avg.update(ppl);
+        // Update status
+        loss_avg.update(loss.array().exp()); // Update with PPL
         bar.set_position((total_len - left) as u64);
         bar.set_message(format!("PPL: {:.2}", loss_avg.value));
         tensorboard.record("ppl", loss_avg.value, BATCH_SIZE);
 
+        // Backprop and optimize
         gradients = Some(loss.backward());
-
+        #[allow(clippy::modulo_one)]
         if left % BATCH_ACCUM == 0 {
-            if let Some(mut gradients) = gradients.take() {
+            if let Some(mut gradients) = Option::take(&mut gradients) {
                 if let Err(e) = opt.update(model, &gradients) {
-                    println!("Update error: {e:?}");
-                    println!("Update error: {e:?}");
-                    println!("Update error: {e:?}");
-
+                    println!("{} {e:?}\n", "Update Error:".bold().red());
                 }
                 model.zero_grads(&mut gradients);
             }
         }
 
-        if tensorboard.iter * BATCH_SIZE % 100_000 == 0 {
-            if let Err(e) = model.save(&format!("step_{}.npz", tensorboard.iter * BATCH_SIZE)) {
-                println!("Error saving model: {e:?}");
+        // Save every 200_000 samples
+        if tensorboard.iter % 200_000 == 0 {
+            if let Err(e) = model.save(&format!("checkpoints/step_{}.npz", tensorboard.iter)) {
+                println!("{} {e:?}\n", "Error Saving Model:".bold().red());
             }
+
+            // Run generation
+            generate(model, dev, 50);
         }
     }
     drop(bar);
@@ -160,9 +166,21 @@ where
     let mut loss_avg = ExponentialAverage::<f32>::new();
     let mut losses = Vec::with_capacity(total_len);
     for ((input, target), left) in dataset.iter_len() {
-        let output = model.forward(dev.tensor(input));
+        let output = match model.try_forward(dev.tensor(input)) {
+            Ok(o) => o,
+            Err(e) => {
+                println!("Forward Error: {e:?}");
+                continue;
+            }
+        };
 
-        let loss: Tensor<(), f32, D> = cross_entropy_with_logits_loss(output, one_hot_encode(&target, dev).unwrap());
+        let loss = match try_cross_entropy_with_logits_loss(output, one_hot_encode(&target, dev).unwrap()) {
+            Ok(l) => l,
+            Err(e) => {
+                println!("Loss Error: {e:?}");
+                continue;
+            }
+        };
         let loss = loss.array().exp();
         losses.push(loss);
         loss_avg.update(loss);
@@ -174,16 +192,15 @@ where
     return losses.iter().sum::<f32>() / (losses.len() as f32)
 }
 
-fn generate<const LAYERS: usize, const VOCAB: usize, const EMBED: usize, const HEADS: usize, D: Device<f32>>(
-    model: &BuiltModel<VOCAB, EMBED, LAYERS, HEADS, 25, f32, D>, 
+fn generate<const LAYERS: usize, const VOCAB: usize, const EMBED: usize, const HEADS: usize, const SEQ: usize, D: Device<f32>>(
+    model: &BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>, 
     dev: &D,
     num_tokens: usize,
 ) 
 where 
-    // D: TensorToArray<(dfdx::shapes::Const<25>, dfdx::shapes::Const<VOCAB>), f32, Array = [[f32; VOCAB]; 25]>,
-    Tensor<(dfdx::shapes::Const<25>, dfdx::shapes::Const<VOCAB>), f32, D>: AsArray<Array = [[f32; VOCAB]; 25]>,
-    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, 25, f32, D>: Module<Tensor<Rank1<25>, usize, D>, Output = Tensor<Rank2<25, VOCAB>, f32, D>>,
-    D: Device<f32> + dfdx::tensor::TensorFrom<[usize; 25], Rank1<25>, usize> + TensorFrom<[[f32; 25]; 25], Rank2<25, 25>, f32>,
+    Tensor<(dfdx::shapes::Const<SEQ>, dfdx::shapes::Const<VOCAB>), f32, D>: AsArray<Array = [[f32; VOCAB]; SEQ]>,
+    BuiltModel<VOCAB, EMBED, LAYERS, HEADS, SEQ, f32, D>: Module<Tensor<Rank1<SEQ>, usize, D>, Output = Tensor<Rank2<SEQ, VOCAB>, f32, D>>,
+    D: Device<f32> + dfdx::tensor::TensorFrom<[usize; SEQ], Rank1<SEQ>, usize> + TensorFrom<[[f32; SEQ]; SEQ], Rank2<SEQ, SEQ>, f32>,
     [(); EMBED * 2]:
 {
     let input = "Hi, how are you doing today? I'd like you to meet my friend Fred. He's a botonist, but also the first man to ever walk on the surface of".to_lowercase();
@@ -192,7 +209,7 @@ where
     let mut indexes = vocab.indexes_from_tokens(&tokens).unwrap();
 
     for _ in 0..num_tokens {
-        let window: [usize; 25] = indexes[indexes.len() - 25..].try_into().unwrap();
+        let window: [usize; SEQ] = indexes[indexes.len() - SEQ..].try_into().unwrap();
         let input: Tensor<_, _, D> = dev.tensor(window);
         let output = model.forward(input);
         let index = output.array().last().unwrap()
@@ -203,7 +220,7 @@ where
             .unwrap();
         indexes.push(index);
     }
-    println!("{} {}", "Generation:".bold(), tokenizer.untokenize(vocab.tokens_from_indexes(&indexes).unwrap()));
+    println!("{} {}\n", "Generation:".bold(), tokenizer.untokenize(vocab.tokens_from_indexes(&indexes).unwrap()));
 }
 
 fn build_dataset<const SEQ: usize, const BATCH: usize>(
@@ -256,7 +273,7 @@ fn build_dataset<const SEQ: usize, const BATCH: usize>(
 fn one_hot_encode<const B: usize, const S: usize, const V: usize, D: Device<f32>>(
     labels: &[[usize; S]; B],
     dev: &D
-) -> Result<Tensor<(Const<B>, Const<S>, Const<V>), f32, D>, D::Err> {
+) -> Result<Tensor<Rank3<B, S, V>, f32, D>, D::Err> {
     let mut data = vec![0.; B * S * V];
     for (b, batch) in labels.iter().enumerate() {
         for (i, l) in batch.iter().enumerate() {
